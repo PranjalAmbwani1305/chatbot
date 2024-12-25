72f1d05d9fb6e86d12fc6501a21eef87301b3ed4
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-25 15:25:34 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -92,7 +92,10 @@ st.title("Chatbot")
 # Cache the chatbot instance
 @st.cache_resource
 def get_chatbot():
-    return CustomChatbot(pdf_path='chatbot/gpmc.pdf')
+    loader = PyMuPDFLoader('gpmc.pdf') 
+    documents = loader.load()
+
+    return CustomChatbot(pdf_path)
 
 
 def generate_response(input_text):
